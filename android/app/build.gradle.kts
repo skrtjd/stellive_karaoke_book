40da99655e06dd80a8b4ee97344ce76fe3bdeb6e
skrtjd <147685812+skrtjd@users.noreply.github.com>
2025-12-13 00:51:56 +0900

    Add files via upload

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,62 @@
+// 👇 항상 import 가 제일 먼저!
+import java.io.FileInputStream
+import java.util.Properties
+
+// 👇 그 다음에 keystore 설정 코드
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.stella_karaoke"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        // 이건 경고라 그냥 두어도 빌드는 됨
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.stella_karaoke"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/com/example/stella_karaoke/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.stella_karaoke
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
